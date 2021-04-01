67c4c6390ab9c954a91c4c1c4bbf4a941b74dc2c
hamzsait <hamzasait@utexas.edu>
2021-04-01 15:45:41 -0500

    added a basic table

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,27 @@
-var currentDay = document.getElementById("currentDay")
+$('#currentDay').text(moment().format("LL"))
+$('#currentDay').css("font-size", "25px")
 
-currentDay.textContent = moment().format("LL")
-currentDay.style.fontSize = "25px"
+for (x = 0; x < 10; x++){
+
+    var time = 9
+
+    var table = $('#table')
+
+    var row = $('<tr>')
+    
+    col1 = $('<td>').text("Hello")
+    col2 = $('<td>').text("Hello")
+    col3 = $('<td>').text("Hello")
+
+    col1.attr("id","col1")
+    col2.attr("id","col2")
+    col3.attr("id","col3")
+
+    row.append(col1)
+    row.append(col2)
+    row.append(col3)
+
+    table.append(row)
+}
+
+
