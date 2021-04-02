8c0cf9cbcb295625a7a2dab1b80d3ae9062ad3b3
hamzsait <hamzasait@utexas.edu>
2021-04-01 19:02:30 -0500

    refactored the for loop

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,19 @@
 $('#currentDay').text(moment().format("LL"))
 $('#currentDay').css("font-size", "25px")
 
+var table = $('#table')
 var time = 9
 currentFound = false
 for (x = 0; x < 8; x++){
 
-    var table = $('#table')
+    
     var row = $('<tr>').css("height","60px")
     
     col1 = $('<td>').text(time + ":00")
     col2 = $('<td>')
     col3 = $('<td>')
 
-    col2.append($('<input>'))
+    col2.append($('<input>').text("lol no way"))
     col3.append($('<button>').text("LOCK"))
 
     col1.attr("id","col1")
@@ -28,9 +29,7 @@ for (x = 0; x < 8; x++){
     if (time >= 12){
         time = 1
     }
-    time++
 
-    
     if (moment().format('h') == time){
         col2.children().css("background-color","red")
         col2.css("background-color","red")
@@ -44,6 +43,13 @@ for (x = 0; x < 8; x++){
         col2.children().css("background-color","gray")
         col2.css("background-color","gray")
     }
+
+    console.log(moment().format('h'))
+
+    time++
+
+    
+    
 }
 
 
